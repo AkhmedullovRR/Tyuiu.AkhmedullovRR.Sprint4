e8a5565a288478af48e8dbededa61d6ae12ca700
Равиль <fullbotrr@mail.ru>
2024-11-13 02:39:13 +0300

    Add project files.

---
+++ Tyuiu.AkhmedullovRR.Sprint4.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.AkhmedullovRR.Sprint4.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.AkhmedullovRR.Sprint4.Task0.V13/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.AkhmedullovRR.Sprint4.Task0.V13.Lib;
+namespace Tyuiu.AkhmedullovRR.Sprint4.Task0.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Ахмедуллов Р. Р. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Ахмедуллов Равиль Радикович | ИСТНб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму          *");
+            Console.WriteLine("* четных элементов массива.   {2, 6, 2, 3, 4, 5, 4, 9, 7, 8}              *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+
+            Console.WriteLine("Исходныйы массив = " + numsArray);
+            for (int i = 0; i < numsArray.Length; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.GetSumEvenArrEl(numsArray);
+
+            Console.WriteLine("Сумма всех четных элементов массива = " + result);
+            Console.ReadKey();
+        }
+    }
+}
